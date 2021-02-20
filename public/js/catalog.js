1ddde77f07db10fd2cac0071aae32d183d60fd56
Aleksandr Novikov <novikovav2@gmail.com>
2021-02-20 15:39:05 +0400

    Homework3

--- public/js/catalog.js
+++ public/js/catalog.js
@@ -1,9 +1,14 @@
 class Catalog {
     _products = []; // массив объектов класса Product
     _placeForRender = '';
+    _cart = null;
+    _numData = 1;
 
-    constructor(placeForRender) {
+    constructor(placeForRender, cart) {
         this._placeForRender = placeForRender;
+        this._cart = cart;
+
+        this.fetchProducts(this._numData);
     }
 
     render() {
@@ -20,9 +25,59 @@ class Catalog {
             `;
             this._placeForRender.appendChild(productElement);
         });
+
+        if (this._numData < 3) {
+            this._placeForRender.insertAdjacentHTML('beforeend',
+                "<button id=\"loadData\">Load more data</button>");
+            this.addListenerLoadData();
+        }
+        this.addListenerAddToCard();
     }
 
-    addProduct(product) {
-        this._products.push(product);
+    addProducts(products) {
+        this._products = products;
+    }
+
+    fetchProducts(num) {
+        const url = `/db/data${num}.json`;
+        return fetch(url)
+            .then(response => response.json())
+            .then((data) => {
+                let fetchProducts = [];
+                fetchProducts = data.data.map(item => {
+                    return new Product(item);
+                });
+                this._products = this._products.concat(fetchProducts);
+                this.render();
+            });
+    }
+
+    addListenerAddToCard() {
+        document.querySelectorAll('.catalog_add-to-card-button')
+            .forEach((button) => {
+                button.addEventListener('click', (event) => {
+                    const product = this.findProductById(event.target.dataset.id, this._products)
+                    if (product) {
+                        this._cart.addProduct(product);
+                        this._cart.render();
+                    }
+                })
+            })
+    }
+
+    addListenerLoadData() {
+        document.querySelector('#loadData')
+            .addEventListener('click', () => {
+                this._numData++;
+                this.fetchProducts(this._numData);
+            })
+    }
+
+    findProductById(id, products) {
+        for (let i=0; i< products.length; i++) {
+            if (products[i].getId() === id) {
+                return products[i];
+            }
+        }
     }
 }
--- public/js/shop.js
+++ public/js/shop.js
@@ -1,8 +1,6 @@
 class Shop {
     _cart = '';
     _catalog = '';
-    _products = [];
-
 
     constructor(placeForRender) {
         const cartElement = document.createElement("div");
@@ -13,62 +11,8 @@ class Shop {
         catalogElement.classList.add('catalog');
         placeForRender.appendChild(catalogElement);
 
-
-        this._products = this.generateProducts()
-        this._catalog = new Catalog(catalogElement);
         this._cart = new Cart(cartElement);
-
-        this.initialLoadProductsToCatalog(this._products, this._catalog);
-
-        this.render();
-
-        this.addListenerAddToCard();
-    }
-
-    addListenerAddToCard() {
-        document.querySelectorAll('.catalog_add-to-card-button')
-            .forEach((button) => {
-            button.addEventListener('click', (event) => {
-                const product = this.findProductById(event.target.dataset.id, this._products)
-                if (product) {
-                    this._cart.addProduct(product);
-                    this._cart.render();
-                }
-            })
-        })
-    }
-
-    render() {
-        this._cart.render();
-        this._catalog.render();
-    }
-
-    generateProducts() {
-        return [
-            new Product('Product 1', 10, '/img/product1.png'),
-            new Product('Product 2', 20, '/img/product2.png'),
-            new Product('Product 3', 30, '/img/product3.png'),
-            new Product('Product 4', 40, '/img/product4.png'),
-            new Product('Product 5', 50, '/img/product5.png'),
-            new Product('Product 6', 60, '/img/product6.png'),
-            new Product('Product 7', 70, '/img/product7.png'),
-            new Product('Product 8', 80, '/img/product8.png'),
-            new Product('Product 9', 90, '/img/product9.png'),
-        ];
-    }
-
-    initialLoadProductsToCatalog(products, catalog) {
-        products.forEach((product) => {
-            catalog.addProduct(product);
-        })
-    }
-
-    findProductById(id, products) {
-        for (let i=0; i< products.length; i++) {
-            if (products[i].getId() === id) {
-                return products[i];
-            }
-        }
+        this._catalog = new Catalog(catalogElement, this._cart);
     }
 }
 
